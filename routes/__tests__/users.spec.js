a2afb542c66462f9904fd4d148a07737b7b3563a
Sarah <sarahennis805@hotmail.com>
2019-12-19 13:16:05 +0000

    Add test: [GET] /api/users returns status 200

--- routes/__tests__/users.spec.js
+++ routes/__tests__/users.spec.js
@@ -37,10 +37,10 @@ describe("GET /api/users/:userId", () => {
   });
 });
 
-// describe("[GET] /api/users endpoint", () => {
-//   test("should return status 200", async () => {
-//     const response = await request(server).get("/api/users");
-//     expect(response.status).toBe(200);
-//     expect(response.body).toBeInstanceOf(Object);
-//   });
-// });
+describe("[GET] /api/users endpoint", () => {
+  test("should return status 200", async () => {
+    const response = await request(server).get("/api/users");
+    expect(response.status).toBe(200);
+    expect(response.body).toBeInstanceOf(Object);
+  });
+});
