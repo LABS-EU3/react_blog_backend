29d4d0690732b207da86fc630e0aaf884da578f5
Sarah <sarahennis805@hotmail.com>
2019-12-19 13:59:19 +0000

    Add migration: Alter users table to contain avatar url column

---
+++ data/migrations/20191219134805_add-img-col-users-tbl.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex) {
+  return knex.schema.table("users", table => {
+      table.string("avatarUrl");
+  })
+};
+
+exports.down = function(knex) {
+    return knex.schema.table("users", table => {
+        table.dropColumn("avatarUrl");
+    })
+};
