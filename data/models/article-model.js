8a880b5ae48b0c2ef0ad6707b4d85a023db5e91c
David Kuseh <davegru203@gmail.com>
2019-12-20 07:19:08 +0000

    specify id  in select method

--- data/models/article-model.js
+++ data/models/article-model.js
@@ -3,7 +3,7 @@ const db = require("../dbConfig");
 async function getArticles() {
     try {
         const response = await db("articles as a")
-        .select("id","title","body","authorId","createdAt","updatedAt")
+        .select("a.id","title","body","authorId","createdAt","updatedAt")
         .where("a.authorId","=", "u.id")
         .leftJoin("users as u", "u.id", "=", "a.authorId")
         return response;
