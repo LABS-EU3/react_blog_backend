d3bcfa330fa7e8199892d00c39f2cc1ca113f078
Uzoamaka126 <vanessapeters126@gmail.com>
2019-12-20 11:12:05 +0100

    Added the initial api message here

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ const { handleError } = require('./utils/errorHandler')
 function routes(app) {
   app.use('/api/auth', auths);
   app.use(handleError);
+  app.get('/', (req, res) => {
+    res.status(200).json({
+        api: "running"
+    })
+  });
 }
 
 module.exports = routes;
