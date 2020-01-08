b4d31f764a671010f58e415ac87624852a98151f
Johnson Ogwuru <ogwurujohnson@gmail.com>
2020-01-08 13:08:58 +0100

    ft: instantiate the AWS sdk with its config details

--- services/articles.js
+++ services/articles.js
@@ -1,4 +1,13 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
 const articles = require("../data/models/article-model");
+const config = require('../config');
+
+const s3 = new AWS.S3({
+  accessKeyId: config.AWS_ACCESS_KEY_ID,
+  secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
+  region: config.AWS_REGION
+});
 
 async function findArticles() {
   const allArticles = await articles.getArticles();
