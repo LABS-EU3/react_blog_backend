645ae06e38a7435a31f1400e886e7241e982fb1e
David Kuseh <davegru203@gmail.com>
2020-01-09 10:13:31 +0000

    run migration to create article-tags table, make changes to tags table

--- data/migrations/20200108105044_tags-table.js
+++ data/migrations/20200108105044_tags-table.js
@@ -1,13 +1,15 @@
 
-exports.up = function(knex) {
-    return knex.schema.createTable("tags", table => {
-        table.increments();
-        table.string("name", 128).notNullable().unique();
-        table.integer("articleId").unsigned().notNullable().references("id").inTable("articles").onUpdate("CASCADE").onDelete("CASCADE");
-    })
-  };
-  
-  exports.down = function(knex) {
-    return knex.schema.dropTableIfExists("tags");
-  };
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("tags", table => {
+    table
+      .increments();
+    table
+      .string("name", 128)
+      .notNullable()
+      .unique();
+  })
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("tags");
+};
