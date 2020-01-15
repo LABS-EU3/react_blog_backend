42109e664b541f3169867cfcda35fd85527e0667
Sarah <sarahennis805@hotmail.com>
2020-01-15 08:38:21 +0000

    Add test: endpoint returns 200 and various arrays regardless of whether user id is present

--- routes/__tests__/articles.spec.js
+++ routes/__tests__/articles.spec.js
@@ -1,31 +1,19 @@
-// const request = require("supertest");
-// const server = require("../../app");
-// const db = require("../../data/dbConfig");
+const request = require("supertest");
+const server = require("../../app");
+const db = require("../../data/dbConfig");
 
-// // beforeAll(async () => {
-// //     await db("articles").truncate();
-// // })
+beforeAll(async () => {
+  await db("articles").delete();
+});
 
-// // afterAll(async () => {
-// //     await db("articles").truncate();
-// // });
+afterAll(async () => {
+  await db("articles").delete();
+});
 
-// describe("GET /api/articles", () => {
-//     test("response should be empty", async () => {
-
-//         const response = await request(server)
-//             .get("/api/articles")
-
-//         expect(response.status).toBe(404)
-//         expect(response.body).toBeInstanceOf(Object)
-
-//     })
-// })
-
-describe("server", () => {
-    describe("[GET] / endpoint", () => {
-      test("the db env is testing", () => {
-        expect(process.env.DB_ENV).toBe("testing");
-      });
-    });
+describe("GET /api/articles", () => {
+  test("response should return 200 an object containing arrays of articles", async () => {
+    const response = await request(server).get("/api/articles");
+    expect(response.status).toBe(200);
+    expect(response.body).toBeInstanceOf(Object);
   });
+});
