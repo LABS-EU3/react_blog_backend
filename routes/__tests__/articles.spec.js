ae9807fa07688a9d39b55e9d58376ab8f96695d9
Sarah <sarahennis805@hotmail.com>
2020-01-15 09:17:34 +0000

    Add test: Should return array of trending articles and array for main feed if credentials are present but user does not have interests or follow any other authors

--- routes/__tests__/articles.spec.js
+++ routes/__tests__/articles.spec.js
@@ -11,14 +11,33 @@ afterAll(async () => {
 });
 
 describe("GET /api/articles", () => {
-  test("response should return 200 an object containing arrays of articles", async () => {
+  test("should return 200 an object containing arrays of articles", async () => {
     const response = await request(server).get("/api/articles");
     expect(response.status).toBe(200);
     expect(response.body).toBeInstanceOf(Object);
   });
-  test("response should always return array of trending articles, regardless of whether credentials are present", async () => {
+  test("Should return array of trending articles and array for main feed if no credentials are present", async () => {
     const response = await request(server).get("/api/articles");
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty("trending");
+    expect(response.body).toHaveProperty("mainFeed");
+  });
+  test("Should return array of trending articles and array for main feed if credentials are present but user does not have interests or follow any other authors", async () => {
+    const mockUserData = {
+      email: "test1000@yahoo.com",
+      password: "1234",
+      fullname: "Test User"
+    };
+    const signUpUserResponse = await request(server)
+      .post("/api/auth/register")
+      .send(mockUserData);
+    const token = signUpUserResponse.body.token;
+
+    const getArticlesResponse = await request(server)
+      .get("/api/articles")
+      .set("Authorization", token);
+    expect(getArticlesResponse.status).toBe(200);
+    expect(getArticlesResponse.body).toHaveProperty("trending");
+    expect(getArticlesResponse.body).toHaveProperty("mainFeed");
   });
 });
