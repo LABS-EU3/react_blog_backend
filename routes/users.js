a59bf090c205355138c90d5265a54b0e0b5bdcbb
Megan Ennis <Megan@Megans-iMac-9.local>
2020-01-21 21:31:19 +0000

    Refactor update users endpoint to allow user to upload profile image. Refactor endpoint to return users new profile info in response

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const service = require("../services/users");
+const { uploadFile } = require("../services/articles");
 const router = express.Router();
+const formidable = require("formidable");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -22,14 +24,29 @@ router.get("/:userId", async (req, res, next) => {
 });
 
 router.put("/:userId", async (req, res, next) => {
-  try {
-    const { userId } = req.params;
-    const { body } = req;
-    const result = await service.editUserInfo(body, userId);
-    res.status(result.statusCode).json(result.data);
-  } catch (error) {
-    next(error);
-  }
-})
+  let form = new formidable.IncomingForm();
+  form.parse(req, async function(err, fields, files) {
+    let result = "";
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    if (files.image) {
+      result = await uploadFile(files.image);
+    }
+    try {
+      let { userId } = req.params;
+      let body = { ...fields };
+      if(result) {
+        body.avatarUrl = result
+      }
+      const response = await service.editUserInfo(body, userId);
+      const newUserData = await service.getUserInfo(userId);
+      res.status(response.statusCode).json(newUserData.data);
+    } catch (error) {
+      next(error);
+    }
+  });
+});
 
 module.exports = router;
