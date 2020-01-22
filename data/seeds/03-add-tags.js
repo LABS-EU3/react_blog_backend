1c65dae7183331719ea12048d63a7e915d284f0c
Johnson Ogwuru <ogwurujohnson@gmail.com>
2020-01-22 02:46:01 +0100

    update seed file

--- data/seeds/03-add-tags.js
+++ data/seeds/03-add-tags.js
@@ -4,13 +4,13 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('tags').insert([
-        {id: 1,  name: 'Business', articleId: 1},
-        {id: 2,  name: 'Tech', articleId: 1},
-        {id: 3,  name: 'Health & Wellbeing', articleId: 2},
-        {id: 4,  name: 'Sport', articleId: 1},
-        {id: 5,  name: 'Cooking', articleId: 2},
-        {id: 6,  name: 'Cooking', articleId: 3},
-        {id: 7,  name: 'Arts & Crafts', articleId: 3}
+        {name: 'Business', articleId: 15},
+        {name: 'Tech', articleId: 15},
+        {name: 'Health & Wellbeing', articleId: 15},
+        {name: 'Sport', articleId: 15},
+        {name: 'Cooking', articleId: 15},
+        {name: 'Cooking', articleId: 15},
+        {name: 'Arts & Crafts', articleId: 15}
       ]);
     });
 };
