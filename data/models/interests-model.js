7dc2f7dcbf7b11421f1227c3510d2da9f4bb6115
Megan Ennis <Megan@Megans-iMac-9.local>
2020-01-23 11:33:28 +0000

    Add model to remove interest by userId and interest name

--- data/models/interests-model.js
+++ data/models/interests-model.js
@@ -1,34 +1,50 @@
 const db = require("../dbConfig");
 
 async function addUserInterests(interests) {
-    try {
-      const [id]= await db("interests").insert(interests, "id");
-      const response = await findUsersByInterest(id)
-      return response;
-    } catch (error) {
-      console.log(error)
-    }
+  try {
+    const [id] = await db("interests").insert(interests, "id");
+    const response = await findUsersByInterest(id);
+    return response;
+  } catch (error) {
+    console.log(error);
   }
-  
-  async function findUsersByInterest(id) {
-    try {
-      const response = await db("interests")
-        .where({id: id})
-        .first()
-        return response;
-    } catch (error) {
-      console.log(error)
-    }
-  }
+}
 
-  async function findInterests() {
-    try {
-      const interests = await db("interests")
-        .select("id","name","userId")
-        return interests;
-    } catch (error) {
-      console.log(error)
-    }
+async function findUsersByInterest(id) {
+  try {
+    const response = await db("interests")
+      .where({ id: id })
+      .first();
+    return response;
+  } catch (error) {
+    console.log(error);
   }
+}
 
-  module.exports = { findInterests, addUserInterests, findUsersByInterest};
+async function findInterests() {
+  try {
+    const interests = await db("interests").select("id", "name", "userId");
+    return interests;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function deleteUserInterest(userId, name) {
+  try {
+    const response = await db("interests")
+      .where({ userId })
+      .andWhere({ name })
+      .del();
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  findInterests,
+  addUserInterests,
+  findUsersByInterest,
+  deleteUserInterest
+};
