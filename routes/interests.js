6facf7c6a6d46ab6daf9e19b9e60a0314430e9af
Megan Ennis <Megan@Megans-iMac-9.local>
2020-01-23 12:44:54 +0000

    Refactor DELETE interests route to plug in authenticate middleware and retrieve token from req auth header. Refactor to accept array of interests in req body

--- routes/interests.js
+++ routes/interests.js
@@ -31,24 +31,18 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.delete("/", checkUserExistsById, async (req, res) => {
-  const { userId, name } = req.body;
-  if (!userId || !name) {
-    res
-      .status(400)
-      .json({ message: "Please provide userId and name in request body" });
-  }
+router.delete("/", authenticate, async (req, res, next) => {
+  const interests = req.body;
   try {
-    const result = await service.removeInterest(userId, name);
-    if (result) {
-      res
-        .status(200)
-        .json({ message: "Successfully deleted", userId, interest: name });
-    } else {
-      res.status(400).json({ error: "Interest does not exist for this user" });
-    }
+      const userId = req.user.subject;
+      interests.map((interest) => {
+          service.removeInterest(userId, interest)
+      })
+    return res.status(200).json({message: "Successfully deleted", userId, interests})
+
   } catch (error) {
-    res.status(500).json({ error: error.message });
+      console.log(error)
+    next(error)
   }
 });
 
