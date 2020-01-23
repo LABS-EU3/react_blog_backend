f145e94215985731034eb0fc495e8e38300cc653
Johnson Ogwuru <ogwurujohnson@gmail.com>
2020-01-23 01:01:52 +0100

    modify seed file

--- data/seeds/03-add-tags.js
+++ data/seeds/03-add-tags.js
@@ -4,13 +4,13 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('tags').insert([
-        {name: 'Business', articleId: 15},
-        {name: 'Tech', articleId: 15},
-        {name: 'Health & Wellbeing', articleId: 15},
-        {name: 'Sport', articleId: 15},
-        {name: 'Cooking', articleId: 15},
-        {name: 'Cooking', articleId: 15},
-        {name: 'Arts & Crafts', articleId: 15}
+        {name: 'Business', articleId: 8},
+        {name: 'Tech', articleId: 8},
+        {name: 'Health & Wellbeing', articleId: 8},
+        {name: 'Sport', articleId: 8},
+        {name: 'Cooking', articleId: 8},
+        {name: 'Cooking', articleId: 8},
+        {name: 'Arts & Crafts', articleId: 8}
       ]);
     });
 };
