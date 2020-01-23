d0349a851f1589bcd9f15be15d116827080a4f91
Megan Ennis <Megan@Megans-iMac-9.local>
2020-01-23 11:34:15 +0000

    Add service to delete interest by user id and interest name

--- services/interests.js
+++ services/interests.js
@@ -1,22 +1,30 @@
 const interest = require("../data/models/interests-model");
 
 async function addInterests(interests) {
-  const newInterest = await interest.addUserInterests(interests)
-   return newInterest;
+  const newInterest = await interest.addUserInterests(interests);
+  return newInterest;
 }
 
 async function getInterests(id) {
   const interests = await interest.findInterests(id);
 
   if (!interests) {
-    return { statusCode: 404, data: { message: "No interests found", interests } };
+    return {
+      statusCode: 404,
+      data: { message: "No interests found", interests }
+    };
   } else {
     return { statusCode: 200, data: { interests } };
   }
 }
 
+async function removeInterest(userId, name) {
+  const response = await interest.deleteUserInterest(userId, name);
+  return response;
+}
 
 module.exports = {
-    addInterests,
-    getInterests
-}
+  addInterests,
+  getInterests,
+  removeInterest
+};
