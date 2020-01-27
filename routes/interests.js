0c65f36c49ca2ce81bdde480710870294d33e23f
Megan Ennis <Megan@Megans-iMac-9.local>
2020-01-27 17:46:33 +0000

    Refactor route to pass entire req body to service, rather than map. Returns array of deleted interests & array of new interests in response

--- routes/interests.js
+++ routes/interests.js
@@ -34,14 +34,23 @@ router.delete("/", authenticate, async (req, res, next) => {
   const interests = req.body;
   try {
     const userId = req.user.subject;
-    const remove = interests.map(async interest => {
-      await service.removeInterest(userId, interest);
-    });
-    if (remove.length === interests.length) {
-      const newInterests = await service.getInterestsByUserId(userId);
+    const remove = await service.removeInterests(userId, interests);
+    const response = await service.getInterestsByUserId(userId);
+    if (remove === interests.length) {
+      return res.status(200).json({
+        message: "Successfully deleted",
+        userId,
+        deleted: interests,
+        new: response
+      });
+    } else {
       return res
-        .status(200)
-        .json({ message: "Successfully deleted", userId, newInterests });
+        .status(404)
+        .json({
+          message: "User does not have interests provided in req body",
+          userId,
+          interests
+        });
     }
   } catch (error) {
     console.log(error);
