e60c40661ad2790b08921d1c1f9c2028918fc029
Francis Bulus <notchera@gmail.com>
2020-01-28 20:34:14 +0100

    complete search feature on the backend

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,26 @@
-const auths = require('./auths');
+const auths = require("./auths");
 const users = require("./users");
-const articles = require('./articles');
-const interests = require('./interests');
-const reactions = require('./reactions')
-const follows = require('./follows');
-const { handleError } = require('./utils/errorHandler')
+const articles = require("./articles");
+const interests = require("./interests");
+const reactions = require("./reactions");
+const follows = require("./follows");
+const search = require("./search");
+const { handleError } = require("./utils/errorHandler");
 
 function routes(app) {
-  app.use('/api/auth', auths);
+  app.use("/api/auth", auths);
   app.use("/api/users", users);
-  app.use('/api/articles', articles);
-  app.use('/api/interests', interests);
-  app.use('/api/follows', follows);
-  app.use('/api/reactions', reactions);
+  app.use("/api/articles", articles);
+  app.use("/api/interests", interests);
+  app.use("/api/follows", follows);
+  app.use("/api/reactions", reactions);
+  app.use("/api/search", search);
   app.use(handleError);
 
-  app.get('/', (req, res) => {
+  app.get("/", (req, res) => {
     res.status(200).json({
-        api: "running"
-    })
+      api: "running"
+    });
   });
 }
 
--- routes/search.js
+++ routes/search.js
@@ -5,20 +5,20 @@ const router = express.Router();
 const { getArticles } = article_services;
 const { getUsers } = user_services;
 
-router.post("/", async (req, res) => {
+router.get("/", async (req, res) => {
   const query = req.query.resources;
+  const regex = new RegExp(query, "i");
   try {
-    const results = await Promise.all([
-      getUsers().data.data,
-      getArticles().data.data
-    ]);
-    const users_results = results[0].filter(user =>
-      new RegExp(query).test(user.full_name)
+    const results = await Promise.all([getUsers(), getArticles()]);
+    const users = results[0].data.data;
+    const articles = results[1];
+    const filtered_users_result = users.filter(user =>
+      regex.test(user.fullname)
     );
-    const aritlces_results = results[1].filter(article =>
-      new RegExp(query).test(article.title)
+    const filtered_articles_result = articles.filter(article =>
+      regex.test(article.title)
     );
-    const response = users_results.concat(aritlces_results);
+    const response = filtered_users_result.concat(filtered_articles_result);
     return res.status(200).json(response);
   } catch (error) {
     res.status(500).json({
