c23572c4d95d0044d8aca3fde541987df0fa9616
Francis Bulus <notchera@gmail.com>
2020-01-28 16:17:52 +0100

    build out queries for articles and users when searched

--- routes/search.js
+++ routes/search.js
@@ -1,15 +1,23 @@
 const express = require("express");
-const article_services = require('../services/articles');
-const user_services = require('../services/users');
+const article_services = require("../services/articles");
+const user_services = require("../services/users");
 const router = express.Router();
-const { } = article_services
-const { } = user_services
+const { getArticles } = article_services;
+const { getUsers } = user_services;
 
 router.post("/", async (req, res) => {
   const query = req.query.resources;
   try {
-    const response = await service.addNewReaction(reaction);
-    return res.status(201).json(response);
+    const users = (await getUsers()).data.data;
+    const users_results = users.filter(user =>
+      new RegExp(query).test(user.full_name)
+    );
+    const articles = (await getArticles()).data.data;
+    const aritlces_results = articles.filter(article =>
+      new RegExp(query).test(article.title)
+    );
+
+    console.log(users, articles, query, users_results, aritlces_results);
   } catch (error) {
     res.status(500).json({
       error: error.message
