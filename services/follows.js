342b710121c7cc8eafcbdc8217fd5da81c0aa062
Megan Ennis <Megan@Megans-iMac-9.local>
2020-01-29 12:07:26 +0000

    Add service to return array of users based on given users interests, if any, if not return all users

--- services/follows.js
+++ services/follows.js
@@ -1,10 +1,32 @@
 const follows = require("../data/models/follow-model");
+const { getInterests } = require("../data/models/user-model");
+const { getUsers } = require("./users");
+const interests = require("../data/models/interests-model");
 
 async function addFollower(follow) {
-    const newFollow = await follows.addNewFollower(follow)
-    return newFollow;
+  const newFollow = await follows.addNewFollower(follow);
+  return newFollow;
+}
+
+async function getUsersToFollow(userId) {
+    try {
+    let usersToFollow;
+    const myInterests =  await getInterests(userId);
+    if (myInterests.length) {
+      const removeNesting = [];
+      myInterests.map(interest => removeNesting.push(interest.name));
+      const users = await interests.findUsersByInterestName(removeNesting);
+      usersToFollow = users.filter(user => user.id !== userId);
+    } else {
+        usersToFollow = await getUsers();
+    }
+    return usersToFollow;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = {
-    addFollower
-}
+  addFollower,
+  getUsersToFollow
+};
