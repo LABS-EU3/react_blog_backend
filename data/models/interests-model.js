90472d707e32082ab1fb45397b7cc8a03baa440a
Megan Ennis <Megan@Megans-iMac-9.local>
2020-01-29 12:06:05 +0000

    Add db model to get users to follow based on array of interests passed in

--- data/models/interests-model.js
+++ data/models/interests-model.js
@@ -1,34 +1,47 @@
 const db = require("../dbConfig");
 
 async function addUserInterests(interests) {
-    try {
-      const [id]= await db("interests").insert(interests, "id");
-      const response = await findUsersByInterest(id)
-      return response;
-    } catch (error) {
-      console.log(error)
-    }
+  try {
+    const [id]= await db("interests").insert(interests, "id");
+    const response = await findUsersByInterest(id)
+    return response;
+  } catch (error) {
+    console.log(error)
   }
-  
-  async function findUsersByInterest(id) {
-    try {
-      const response = await db("interests")
-        .where({id: id})
-        .first()
-        return response;
-    } catch (error) {
-      console.log(error)
-    }
-  }
+}
 
-  async function findInterests() {
-    try {
-      const interests = await db("interests")
-        .select("id","name","userId")
-        return interests;
-    } catch (error) {
-      console.log(error)
-    }
+async function findUsersByInterest(id) {
+  try {
+    const response = await db("interests")
+      .where({id: id})
+      .first()
+    return response;
+  } catch (error) {
+    console.log(error)
   }
+}
 
-  module.exports = { findInterests, addUserInterests, findUsersByInterest};
+async function findUsersByInterestName(interests) {
+  try {
+    const userIds = await db("interests as i")
+      .select("u.id", "u.fullname", "u.bio", "u.avatarUrl")
+      .join("users as u", "u.id", "i.userId")
+      .whereIn("i.name", interests)
+      .distinct();
+    return userIds;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function findInterests() {
+  try {
+    const interests = await db("interests")
+    .select("id","name","userId")
+    return interests;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = { findInterests, addUserInterests, findUsersByInterest, findUsersByInterestName };
