289ac226b35d73917a6af0a0c65b7bb32ee2cdac
Megan Ennis <Megan@Megans-iMac-9.local>
2020-01-30 10:37:47 +0000

    Refactor route to use authticate middleware and grab user id from req header. Use Promise.all to map over req body and call service to add follow.

--- routes/follows.js
+++ routes/follows.js
@@ -1,18 +1,23 @@
 const express = require("express");
 
 const service = require("../services/follows");
-
+const { authenticate } = require("./utils/loggedIn");
 const router = express.Router();
 
-router.post("/", async (req, res, next) => {
-    const newFollow = {...req.body};
-    try {
-        const response = await service.addFollower(newFollow)
-        res.status(201).json({message: "New follower added", response})
-        return response;
-    } catch (error){
-        next(error)
-    }
-})
+router.post("/", authenticate, async (req, res, next) => {
+  const newFollowIds = req.body;
+  try {
+    const userId = req.user.subject;
+    Promise.all(
+      newFollowIds.map(id => {
+        service.addFollower({ followerId: userId, followingId: id });
+      })
+    )
+      .then(() => res.status(201).json({ message: "New followers added" }))
+      .catch(err => console.log(err));
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
