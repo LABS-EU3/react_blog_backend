8289e24eafe4feeaa5e246eda65e93b674117ed5
Francis Bulus <notchera@gmail.com>
2020-01-30 17:51:25 +0100

    took out unneccessary endpoint in notifications router

--- routes/notifications.js
+++ routes/notifications.js
@@ -4,24 +4,24 @@ const { authenticate } = require("./utils/loggedIn");
 const service = require("../services/notifications");
 const pusher = require("./utils/pusherConfig");
 
-router.post("/", authenticate, async (req, res, next) => {
-  const notification = req.body;
-  try {
-    const userId = req.user.subject;
-    const response = service.addNotification(notification);
-    if (response) {
-      if (response.subjectId === userId) {
-        pusher.trigger(`$notifications-channel-${userId}`, "new-notification", {
-          response
-        });
-      }
-    }
-    return res.status(201).json({ message: "Added successfully" });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
+// router.post("/", authenticate, async (req, res, next) => {
+//   const notification = req.body;
+//   try {
+//     const userId = req.user.subject;
+//     const response = service.addNotification(notification);
+//     if (response) {
+//       if (response.subjectId === userId) {
+//         pusher.trigger(`$notifications-channel-${userId}`, "new-notification", {
+//           response
+//         });
+//       }
+//     }
+//     return res.status(201).json({ message: "Added successfully" });
+//   } catch (error) {
+//     console.log(error);
+//     next(error);
+//   }
+// });
 
 router.get("/", authenticate, async (req, res, next) => {
   const userId = req.user.subject;
