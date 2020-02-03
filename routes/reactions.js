0a0143043ebef73778e3384d1c9fda0be8415571
Johnson Ogwuru <ogwurujohnson@gmail.com>
2020-02-03 14:25:09 +0100

    remove console logs

--- routes/reactions.js
+++ routes/reactions.js
@@ -10,7 +10,6 @@ const articles = require("../data/models/article-model");
 router.post("/", authenticate, async (req, res) => {
   const incoming = req.body;
   const article = await articles.getArticlesById(req.body.articleId)
-  console.log(req.body.articleId)
   const new_notification = {
     type: 'reactions',
     content: req.body.highlighted_text,
